b441ae0816f87a0037adde93c86500023da6a5ff
Marco Pizarro <marcopizarro@users.noreply.github.com>
2021-07-07 13:42:53 -0700

    User Story 4: Make a Post

--- app/src/main/java/com/codepath/instagram/MainActivity.java
+++ app/src/main/java/com/codepath/instagram/MainActivity.java
@@ -1,22 +1,61 @@
 package com.codepath.instagram;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.LogOutCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+
+    private File photoFile;
+    private File resizedFile;
+    public String photoFileName = "photo";
+
+    private BottomNavigationView bottomNavigationView;
+    private Button btnTakePhoto;
+    private Button btnSubmitPost;
+    private ImageView ivImagePreview;
+    private EditText etDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +63,184 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         myToolbar.setLogo(R.drawable.nav_logo_whiteout);
-        myToolbar.setTitle(null);
+        myToolbar.setTitle("");
         setSupportActionBar(myToolbar);
+
+        btnTakePhoto = findViewById(R.id.btnTakePhoto);
+        btnSubmitPost = findViewById(R.id.btnSubmitPost);
+        ivImagePreview = findViewById(R.id.ivImagePreview);
+        etDesc = findViewById(R.id.etDesc);
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
+
+        btnSubmitPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDesc.getText().toString();
+                if (description.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Description can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (photoFile == null || ivImagePreview.getDrawable() == null) {
+                    Toast.makeText(MainActivity.this, "Add an image to continue", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(description, currentUser, photoFile);
+            }
+        });
+//        bottomNavigationView = findViewById(R.id.bottom_navigation);
+//
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                Fragment fragment;
+//                switch (item.getItemId()) {
+//                    case R.id.btnHome:
+//                        Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.btnAdd:
+//                        Toast.makeText(MainActivity.this, "add", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.btnProfile:
+//                        Toast.makeText(MainActivity.this, "prof", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
+    }
+
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    private File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(takenImage, 300);
+                // Configure byte output stream
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                // Compress the image further
+                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+                resizedFile = getPhotoFileUri(photoFileName + "_resized");
+                try {
+                    resizedFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(resizedFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                // Write the bytes of the bitmap to file
+                try {
+                    fos.write(bytes.toByteArray());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivImagePreview.setImageBitmap(resizedBitmap);
+            } else { // Result was a failure
+                Toast.makeText(this, "No picture taken", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setUser(currentUser);
+        post.setImage(new ParseFile(resizedFile));
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(MainActivity.this, "Error Saving", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Log.i(TAG, "Post saved");
+                etDesc.setText("");
+                ivImagePreview.setImageResource(0);
+            }
+        });
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Unable to fetch posts", e);
+                    return;
+                } else {
+                    for (Post post : posts) {
+                        ParseUser author = post.getUser();
+                        try {
+                            Log.i(TAG, "Post: " + author.fetchIfNeeded().getString("username"));
+                        } catch (ParseException f) {
+                            Log.v(TAG, f.toString());
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
 
     }
 
@@ -40,7 +255,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.log_out) {
             ParseUser.logOut();
-            if(ParseUser.getCurrentUser() == null){
+            if (ParseUser.getCurrentUser() == null) {
                 Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
@@ -50,6 +265,38 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+}
+
+class BitmapScaler {
+    // scale and keep aspect ratio
+    public static Bitmap scaleToFitWidth(Bitmap b, int width) {
+        float factor = width / (float) b.getWidth();
+        return Bitmap.createScaledBitmap(b, width, (int) (b.getHeight() * factor), true);
+    }
 
 
+    // scale and keep aspect ratio
+    public static Bitmap scaleToFitHeight(Bitmap b, int height) {
+        float factor = height / (float) b.getHeight();
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factor), height, true);
+    }
+
+
+    // scale and keep aspect ratio
+    public static Bitmap scaleToFill(Bitmap b, int width, int height) {
+        float factorH = height / (float) b.getWidth();
+        float factorW = width / (float) b.getWidth();
+        float factorToUse = (factorH > factorW) ? factorW : factorH;
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factorToUse),
+                (int) (b.getHeight() * factorToUse), true);
+    }
+
+
+    // scale and don't keep aspect ratio
+    public static Bitmap strechToFill(Bitmap b, int width, int height) {
+        float factorH = height / (float) b.getHeight();
+        float factorW = width / (float) b.getWidth();
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factorW),
+                (int) (b.getHeight() * factorH), true);
+    }
 }
--- app/src/main/java/com/codepath/instagram/ParseApplication.java
+++ app/src/main/java/com/codepath/instagram/ParseApplication.java
@@ -3,12 +3,13 @@ package com.codepath.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("BJZuTUWLYwQ5EUY9ocvrSdGHyeoSxUIz7V9zbjHk")
                 .clientKey("NDPlZnE5O78FLYTAWkxzKWWLqAf3IiwT5SYXxfsj")
